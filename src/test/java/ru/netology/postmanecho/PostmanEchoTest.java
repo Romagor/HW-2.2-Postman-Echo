3bdd311c97f1538e7125e993dc18b37e25441842
RomaGor <goryachevskii60@gmail.com>
2022-09-09 16:07:45 +0300

    fix: change test shouldReturnBody

--- src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello!"))
+                .body("data", equalTo("Hello, World!"))
         ;
     }
 }
